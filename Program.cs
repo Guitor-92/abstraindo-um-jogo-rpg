d6e4380bb9d13860a01f529ef57979f15232f9c3
guilhermevitor <vitorguilh@hotmail.com>
2022-05-20 00:19:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using abstraindo_um_jogo_rpg.src.entities;
+
+namespace  abstraindo_um_jogo_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arus hero = new Arus("Arus", 22, "Knight");
+            Arus oponent = new Arus("Jennica", 23, "White Wizard");
+            
+           
+            Console.WriteLine(hero.Attack());
+            Console.WriteLine(oponent.Attack());
+        }
+    }
+}
